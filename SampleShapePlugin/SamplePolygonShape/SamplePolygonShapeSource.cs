6e6455b3ff1c20957f0614c1dda2dcc0032bee61
manju-summoner <manju_summoner@manjubox.net>
2024-08-04 08:07:21 +0900

    図形の各点の数値が同じでインスタンスだけが変わっている場合、制御点が操作不能になる問題を修正

--- SampleShapePlugin/SamplePolygonShape/SamplePolygonShapeSource.cs
+++ SampleShapePlugin/SamplePolygonShape/SamplePolygonShapeSource.cs
@@ -1,4 +1,5 @@
-﻿using System.Numerics;
+﻿using System.Collections.Immutable;
+using System.Numerics;
 using Vortice.Direct2D1;
 using YukkuriMovieMaker.Commons;
 using YukkuriMovieMaker.Player.Video;
@@ -15,6 +16,7 @@ namespace SampleShapePlugin.SamplePolygonShape
         public ID2D1Image Output => commandList ?? throw new InvalidOperationException("commandList is null");
 
         bool isFirst = true;
+        ImmutableList<SamplePolygonShapePoint>? pointList;
         Vector2[] points = [];
 
         readonly ID2D1SolidColorBrush white;
@@ -38,6 +40,7 @@ namespace SampleShapePlugin.SamplePolygonShape
             var length = timelineItemSourceDescription.ItemDuration.Frame;
             var fps = timelineItemSourceDescription.FPS;
 
+            var pointList = samplePolygonShapeParameter.Points;
             var points =
                 samplePolygonShapeParameter.Points
                 .Select(p =>
@@ -47,8 +50,8 @@ namespace SampleShapePlugin.SamplePolygonShape
                 .ToArray();
 
             //変更がない場合は戻る
-            if (!isFirst && this.points.SequenceEqual(points))
-                return;
+            if (!isFirst && this.pointList == pointList && this.points.SequenceEqual(points))
+               return;
 
             //前回のUpdateで作成した図形を破棄して新しい図形を作成する
             if(geometry is not null)
@@ -101,6 +104,7 @@ namespace SampleShapePlugin.SamplePolygonShape
 
             //キャッシュ用の情報を保存しておく
             isFirst = false;
+            this.pointList = pointList;
             this.points = points;
         }
 
